b1a98839aa5a0f29438677cc62fee19ccae244db
Favour Samuel <sfchidimma@gmail.com>
2021-03-01 17:31:00 +0100

    Include navbar with jQuery

--- README.md
+++ README.md
@@ -1,5 +1,59 @@
-# Sample Portfolio
+# Single Page Portfolio
 
-This is just a practice project.
+This is a portfolio for Amara with responsive sticky navigation bar created with jQuery. 
 
-![Amaras Portfolio](screenshot.jpg)
+![Amara's Portfolio](screenshot.jpg)
+## Built With
+- [HTML5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+- [SASS](https://sass-lang.com/)
+- [jQuery](https://jquery.com/)
+
+## Requirements
+ - NPM
+ - Gulp.js
+
+## Install
+```
+    $ git clone git@github.com:samiiexx/single-page-portfolio.git main
+    $ cd main
+    $ npm install
+```
+
+## Running the project
+Production directory is the `public` folder.\
+\
+Run a full build
+```
+gulp all
+```
+Watch files and run BrowserSync
+```
+gulp serve
+```
+### Individual Tasks
+These tasks can be run individually.\
+\
+Include HTML partials
+```
+gulp html
+```
+Compile SASS, browser autoprefix and minify CSS
+```
+gulp style
+```
+Compile, transpile and minify JS
+```
+gulp js
+```
+Minify images
+```
+gulp image
+```
+
+## Contributors
+- Favour Samuel (@samiiexx)
+
+## License
+Copyright (c) 2021 Favour Samuel.
+
+For enquiries please contact me at [favour@starboxtech.com](mailto:favour@starboxtech.com).
--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,115 @@
-// Require plugins
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+// Define plugins
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const fileinclude = require("gulp-file-include");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const browserSync = require("browser-sync").create();
 
-// Source and destination directories
-const srcDir = 'src';
-const destDir = 'public'; // Also known as 'dist' or 'build'
+// Define source and destination directories
+const srcDir = "src";
+const destDir = "public";
 
-// Copy Files
-function copyFiles() {
-    return gulp.src([`${srcDir}/*.html`, `${srcDir}/*.jpg`, `${srcDir}/*.png`])
+// Define gulp const
+const { src, series, parallel, dest, watch } = require("gulp");
+
+// Copy PHP files
+function php() {
+    return src(`${srcDir}/*.php`).pipe(gulp.dest(destDir));
+}
+
+// Include HTML partials
+function html() {
+    return src([`${srcDir}/**/*.html`, `!${srcDir}/includes/**`])
+        .pipe(
+            fileinclude({
+                prefix: "@@",
+                basepath: "@file",
+            })
+        )
         .pipe(gulp.dest(destDir));
 }
 
-function compileSass() {
-    return gulp.src(srcDir + '/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest(destDir + '/css'))
-        .pipe(browserSync.stream());
+// Compile (and minify) SASS
+let sassOptions = {
+    errLogToConsole: true,
+    outputStyle: "expanded", // ['compressed','expanded']
+};
+
+function style() {
+    return src(`${srcDir}/sass/style.scss`)
+        .pipe(sass(sassOptions).on("error", sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(destDir + "/css"));
 }
 
-function watch() {
-    browserSync.init({
+// Compile and Minify JS
+function js() {
+    return src([
+        `${srcDir}/js/jquery-3.5.1.js`,
+        `${srcDir}/js/!(index)*.js`,
+        `${srcDir}/js/index.js`,
+    ])
+        .pipe(
+            babel({
+                presets: ["@babel/env"],
+                compact: false,
+            })
+        )
+        .pipe(concat("index.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest(destDir + "/js"));
+}
+
+// Minify Images
+function image() {
+    return src(`${srcDir}/images/*`)
+        .pipe(
+            imagemin([
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.mozjpeg({ quality: 65, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [{ removeViewBox: true }, { cleanupIDs: true }],
+                }),
+            ])
+        )
+        .pipe(gulp.dest(destDir + "/images"));
+}
+
+function browser() {
+    return browserSync.init({
+        notify: false,
+        // open: false,
         server: {
-            baseDir: destDir
-        }
+            baseDir: destDir,
+        },
     });
-    gulp.watch(srcDir + '/scss/**/*.scss', compileSass);
-    gulp.watch(srcDir + '/**/*.html').on('change', browserSync.reload);
-    gulp.watch(srcDir + '/js/**/*.js').on('change', browserSync.reload);
 }
 
-exports.compileSass = compileSass;
-exports.watch = watch;
-exports.all = gulp.parallel(copyFiles, compileSass);
+// Watch files
+function serve() {
+    browser();
+    watch(`${srcDir}/sass/*`).on("change", series(style, browserSync.reload));
+    watch(`${srcDir}/*.php`).on("change", series(php, browserSync.reload));
+    watch(`${srcDir}/js/*`).on("change", series(js, browserSync.reload));
+    watch(`${srcDir}/includes/*`).on(
+        "change",
+        series(html, browserSync.reload)
+    );
+    watch(`${srcDir}/*.html`).on("change", series(html, browserSync.reload));
+    watch(`${srcDir}/images/*`).on("change", series(image, browserSync.reload));
+}
+
+// Export tasks
+exports.html = html;
+exports.php = php;
+exports.style = style;
+exports.image = image;
+exports.js = js;
+exports.serve = serve;
+exports.browser = browser;
+exports.all = series(php, html, style, js, image);
